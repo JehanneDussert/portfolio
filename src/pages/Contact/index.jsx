a0291657769d71b5476f15c55483fa3306e367ba
Jehanne DUSSERT <jdussert@student.42.fr>
2022-09-06 09:26:13 +0200

    add contact component

---
+++ src/pages/Contact/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { BigCard } from '../../utils/style/Atoms'
+
+function Contact() {
+
+    return (
+        <BigCard>
+            Page contact
+        </BigCard>
+    )
+}
+  
+export default Contact
